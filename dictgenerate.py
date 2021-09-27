b3dc184419c8937eda83dff014def6893237e987
Ethan Jolles <85588248+EthanJolles@users.noreply.github.com>
2021-09-27 00:09:01 -0400

    Add files via upload

---
+++ dictgenerate.py
@@ -0,0 +1,30 @@
+# Question:
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an
+# integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+
+# my solution
+# def generatedict(n):
+#     dictionary = {}
+#     for i in range(1, n+1):
+#         dictionary[i] = i**2
+#
+#     print(dictionary)
+#
+#
+# n = int(input())
+# generatedict(n)
+
+
+# my solution using dictionary comprehension
+def generatedict(n):
+    dictionary = {i: i**2 for i in range(1, n + 1)}
+    print(dictionary)
+
+
+n = int(input())
+generatedict(n)
